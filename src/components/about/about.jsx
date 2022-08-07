48679c9a31b5b5b32c2fe64af0f4fe71a0ba48b2
aryanjain21 <aryanjain1997@gmail.com>
2022-08-07 17:13:31 +0530

    Added resume

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { Box, Link } from "@mui/material";
+import { Box, Button, Link } from "@mui/material";
 import Title from "../../shared/title/title";
 import Aryan from "../../assets/images/aryan.png";
+import resume from '../../assets/Aryan_Jain_Resume.pdf';
 
 const About = () => {
+
   return (
     <Box
       sx={{
@@ -25,53 +27,67 @@ const About = () => {
         <Box
           component={"p"}
           sx={{
-            fontSize: { xs: "1.2rem", sm: '1.3rem', md: "1.2rem" },
+            fontSize: { xs: "1.2rem", sm: "1.3rem", md: "1.2rem" },
             lineHeight: 1.1,
             color: "#fff",
           }}
         >
-          Hi, I'm Aryan Jain.
-          Currently working as Frontend Engineer at Mindstix, Pune.
+          Hi, I'm Aryan Jain. Currently working as Frontend Engineer at
+          Mindstix, Pune.
         </Box>
         <Box
           component="p"
           sx={{
-            fontSize: { xs: "1.2rem", sm: '1.3rem', md: "1.2rem" },
+            fontSize: { xs: "1.2rem", sm: "1.3rem", md: "1.2rem" },
             lineHeight: 1.1,
             color: "#fff",
           }}
         >
-          I have a passion for development, animations and developing responsive website with
-          dynamic user experiences with functionality.
+          I have a passion for development, animations and developing functional & responsive
+          website with dynamic user experiences.
         </Box>
         <Box
           component={"p"}
           sx={{
-            fontSize: { xs: "1.2rem", sm: '1.3rem', md: "1.2rem" },
+            fontSize: { xs: "1.2rem", sm: "1.3rem", md: "1.2rem" },
             lineHeight: 1.1,
             color: "#fff",
           }}
         >
-          Interested in the fullstack development & freelancing as well and working on ambitious
-          projects with positive people. For contact me hit the below link
+          Interested in the fullstack development & freelancing as well and
+          working on ambitious projects with positive people. For contact me hit
+          the below link
         </Box>
-        <Link
-          href="/contact"
+        <Box
           sx={{
-            fontSize: "1.2rem",
-            lineHeight: 1.1,
-            color: "#08fdd8",
-            mt: "16px",
-            textDecoration: "none",
+            display: 'flex',
+            flexDirection: { xs: 'row', md: 'column' },
+            alignItems: { xs: 'center', md: 'flex-start' },
+            mt: "11px",
           }}
         >
-          Invite Me On &#9749;!!
-        </Link>
+          <Link
+            href="/contact"
+            sx={{
+              fontSize: "1.2rem",
+              lineHeight: 1.1,
+              color: "#08fdd8",
+              mr: { xs: '24px', md: 0 },
+              mb: { xs: 0, md: '11px' },
+              textDecoration: "none",
+            }}
+          >
+            Invite Me On &#9749;!!
+          </Link>
+          <Button component='a'  href={resume} download="Aryan_Resume" target='_blank' variant="contained" color="primary">
+            Resume
+          </Button>
+        </Box>
       </Box>
       <Box
         sx={{
-          width: { xs: "100%", sm: '75%', lg: "50%", xl: "40%" },
-          height: { xs: "50%", sm: '56%', md: "500px" },
+          width: { xs: "100%", sm: "75%", lg: "50%", xl: "40%" },
+          height: { xs: "50%", sm: "56%", md: "500px" },
           mx: { xs: 0, lg: "54px" },
           mt: { xs: "24px", md: 0 },
           borderRadius: "100%",
@@ -84,8 +100,8 @@ const About = () => {
           component="img"
           sx={{
             position: "absolute",
-            objectFit: { xs: 'cover', sm: 'normal' },
-            bottom: { xs: 'auto', sm: 0 },
+            // objectFit: { xs: "cover", sm: "normal" },
+            bottom: { xs: "auto", sm: 0 },
             right: { xs: -30, sm: 50 },
           }}
           src={Aryan}
